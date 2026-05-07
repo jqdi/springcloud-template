169939e64326f408100bdcc2044f28221dfb3331
JQ棣 <897665787@qq.com>
2026-05-07 15:23:20 +0800

    升级国际化

--- tool/service/src/main/java/com/company/tool/i18n/CommonI18nDataProvider.java
+++ tool/service/src/main/java/com/company/tool/i18n/CommonI18nDataProvider.java
@@ -1,8 +1,6 @@
 package com.company.tool.i18n;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +11,7 @@ import com.company.tool.entity.CommonI18n;
 import com.company.tool.service.CommonI18nService;
 
 import io.github.jqdi.i18n.core.metadata.I18nFieldInfo;
+import io.github.jqdi.i18n.core.metadata.I18nTableInfo;
 import io.github.jqdi.i18n.core.metadata.RelatedI18nValueMapping;
 import io.github.jqdi.i18n.core.provider.I18nDataProvider;
 
@@ -28,17 +27,32 @@ public class CommonI18nDataProvider implements I18nDataProvider {
     private CommonI18nService commonI18nService;
 
     @Override
-    public List<RelatedI18nValueMapping> getValueMapping(I18nFieldInfo i18nFieldInfo, Set<Object> relatedFieldValueSet) {
-        String i18nTable = i18nFieldInfo.getI18nTable();
-        String i18nColumn = i18nFieldInfo.getI18nColumn();
+    public Map<I18nFieldInfo, List<RelatedI18nValueMapping>> getValueMapping(I18nTableInfo i18nTableInfo,
+                                                                             Set<Object> relatedFieldValueSet) {
+        String i18nTable = i18nTableInfo.getI18nTable();
+        String prefix = i18nTable.replace("_i18n", "");
 
-        String businessType = i18nTable.replace("_i18n", "") + "." + i18nColumn;
+        List<I18nFieldInfo> i18nFieldInfoList = i18nTableInfo.getI18nFieldInfoList();
         List<Integer> businessIdList = relatedFieldValueSet.stream().filter(Objects::nonNull).map(Object::toString)
-            .map(Integer::valueOf).collect(Collectors.toList());
-        List<CommonI18n> commonI18nList = commonI18nService.selectByBusinessTypeBusinessidsLocale(businessType, businessIdList,
-            LocaleContextHolder.getLocale().toLanguageTag());
-        List<RelatedI18nValueMapping> valueMappingList = commonI18nList.stream()
-            .map(v -> new RelatedI18nValueMapping(v.getBusinessId(), v.getI18nText())).collect(Collectors.toList());
-        return valueMappingList;
+                .map(Integer::valueOf).collect(Collectors.toList());
+
+        List<CommonI18n> commonI18nParamList = i18nFieldInfoList.stream().map(I18nFieldInfo::getI18nColumn)
+                .map(v -> i18nTable.replace("_i18n", "") + "." + v).map(businessType -> {
+                    return businessIdList.stream().map(businessId -> {
+                        CommonI18n commonI18n = new CommonI18n();
+                        commonI18n.setBusinessType(businessType);
+                        commonI18n.setBusinessId(businessId);
+                        return commonI18n;
+                    }).collect(Collectors.toList());
+                }).flatMap(Collection::stream).collect(Collectors.toList());
+
+        List<CommonI18n> commonI18nList = commonI18nService.selectByBusinessTypesBusinessIdsLocale(commonI18nParamList,
+                LocaleContextHolder.getLocale().toLanguageTag());
+        return i18nFieldInfoList.stream().collect(Collectors.toMap(f -> f, i18nFieldInfo -> {
+            List<RelatedI18nValueMapping> relatedI18nValueMappingList =
+                    commonI18nList.stream().filter(v -> v.getBusinessType().equals(prefix + "." + i18nFieldInfo.getI18nColumn()))
+                            .map(v -> new RelatedI18nValueMapping(v.getBusinessId(), v.getI18nText())).collect(Collectors.toList());
+            return relatedI18nValueMappingList;
+        }));
     }
 }
