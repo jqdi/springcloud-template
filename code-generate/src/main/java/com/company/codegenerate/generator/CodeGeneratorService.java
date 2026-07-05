67077efc49335c6a0442f2e74907b9c23a3c99f0
JQ棣 <897665787@qq.com>
2026-07-05 20:15:23 +0800

    暂存

--- code-generate/src/main/java/com/company/codegenerate/generator/CodeGeneratorService.java
+++ code-generate/src/main/java/com/company/codegenerate/generator/CodeGeneratorService.java
@@ -1,9 +1,7 @@
 package com.company.codegenerate.generator;
 
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
@@ -34,10 +32,14 @@ public class CodeGeneratorService {
      */
     public void generateCode(String tableName, String moduleName) {
         String projectPath = System.getProperty("user.dir");
-        // String outputPath = Paths.get(projectPath, moduleName).toString();
         String outputPath = Paths.get(projectPath, moduleName, "service").toString();
+//        String apiPath = Paths.get(projectPath, moduleName, "api", "src", "main", "java").toString();
+        String apiPath = "D:\\code\\me\\springcloud-template\\order\\api\\src\\main\\java\\com\\company\\order\\api";
+//        String adminapiPath = Paths.get(projectPath, "adminapi", "src", "main", "java").toString();
+        String adminapiPath = "D:\\code\\me\\springcloud-template\\adminapi\\src\\main\\java\\com\\company\\adminapi";
 
-        log.info("开始生成代码，表名: {}，模块名: {}，输出路径: {}", tableName, moduleName, outputPath);
+        log.info("开始生成代码，表名: {}，模块名: {}，输出路径: {}，api路径: {}，adminapi路径: {}",
+            tableName, moduleName, outputPath, apiPath, adminapiPath);
 
         // 构建数据源配置
         DataSourceConfig.Builder dataSourceConfigBuilder =
@@ -47,12 +49,12 @@ public class CodeGeneratorService {
         FastAutoGenerator.create(dataSourceConfigBuilder)
             // 全局配置
             .globalConfig(builder -> {
-                builder//
+                builder
                     .author("CodeGenerate") // 设置作者
                     .enableSwagger() // 开启 swagger 模式
                     .disableOpenDir() // 禁止打开输出目录
                     .outputDir(outputPath + "/src/main/java") // 指定输出目录
-                    .author("CodeGenerate").dateType(DateType.TIME_PACK) // 时间策略
+                    .dateType(DateType.TIME_PACK) // 时间策略
                 ;
             })
             // 包配置
@@ -60,17 +62,13 @@ public class CodeGeneratorService {
                 Map<OutputFile, String> pathInfo = new HashMap<>();
                 pathInfo.put(OutputFile.xml, outputPath + "/src/main/resources/mapper");
 
-                builder//
-//                    .parent(getPackageParent(moduleName)) // 设置父包名
-//                    .parent("com.company."+moduleName) // 设置父包名
+                builder
                     .parent("com.company") // 设置父包名
-//                    .moduleName(getModuleName(moduleName)) // 设置父包模块名
                     .moduleName(moduleName) // 设置父包模块名
                     .entity("entity") // Entity包名
                     .mapper("mapper") // Mapper包名
                     .xml("mapper.xml") // Mapper XML包名
                     .service("service") // Service包名
-                    // .serviceImpl("service.impl") // Service Impl包名
                     .controller("controller") // Controller包名
                     .pathInfo(pathInfo) // 配置其他路径
                 ;
@@ -80,33 +78,29 @@ public class CodeGeneratorService {
                 builder.addInclude(tableName);
 
                 builder.entityBuilder() // 实体策略配置
-                    .javaTemplate("/templates/entity.java.vm")//
-                    .enableFileOverride()//
+                    .javaTemplate("/templates/entity.java.vm")
+                    .enableFileOverride()
                     .enableLombok() // 开启 Lombok
-                    // .logicDeleteColumnName("deleted") // 逻辑删除字段
                     .build();
 
                 builder.mapperBuilder() // Mapper策略配置
-                    .mapperTemplate("/templates/mapper.java.vm")//
-                    .mapperXmlTemplate("/templates/mapper.xml.vm")//
-                    .enableFileOverride()//
-                    // .enableMapperAnnotation() // 开启@Mapper注解
+                    .mapperTemplate("/templates/mapper.java.vm")
+                    .mapperXmlTemplate("/templates/mapper.xml.vm")
+                    .enableFileOverride()
                     .enableBaseResultMap() // 启用 BaseResultMap 生成
                     .enableBaseColumnList() // 启用 BaseColumnList
                     .build();
 
                 builder.serviceBuilder() // 服务策略配置
-                    .serviceTemplate("/templates/service.java.vm")//
-                    // .serviceImplTemplate("/templates/serviceImpl.java.vm")//
-                    .enableFileOverride()//
+                    .serviceTemplate("/templates/service.java.vm")
+                    .enableFileOverride()
                     .formatServiceFileName("%sService") // service命名方式
-                    .disableServiceImpl()//
-                    // .formatServiceImplFileName("%sServiceImpl") // service impl命名方式
+                    .disableServiceImpl()
                     .build();
 
                 builder.controllerBuilder() // 控制器策略配置
-                    .template("/templates/controller.java.vm")//
-                    .enableFileOverride()//
+                    .template("/templates/controller.java.vm")
+                    .enableFileOverride()
                     .enableRestStyle() // 开启@RestController注解
                     .enableHyphenStyle() // 开启驼峰转连字符
                     .build();
@@ -117,80 +111,58 @@ public class CodeGeneratorService {
                 Map<String, Object> customMap = Maps.newHashMap();
                 customMap.put("lowEntity", "aaaa");
                 customMap.put("apiPackage", "com.company." + moduleName + ".api");
+                customMap.put("adminapiPackage", "com.company.adminapi");
                 consumer.customMap(customMap);
 
-                String apiPath = "D:\\code\\me\\springcloud-template\\order\\api\\src\\main\\java\\com\\company\\order\\api";
-
-                consumer.customFile(builder -> builder//
-                    .templatePath("/templates/entityReq.java.vm")//
-//                    .filePath("D:\\code\\me\\springcloud-template\\order\\api")
+                consumer.customFile(builder -> builder
+                    .templatePath("/templates/entityReq.java.vm")
                     .filePath(apiPath)
-                    .packageName("request")//
-                    .formatNameFunction(tableInfo -> tableInfo.getEntityName() + "Req")//
-                    .fileName(".java")//
-                    .enableFileOverride()//
+                    .packageName("request")
+                    .formatNameFunction(tableInfo -> tableInfo.getEntityName() + "Req")
+                    .fileName(".java")
+                    .enableFileOverride()
                 );
-                consumer.customFile(builder -> builder//
-                    .templatePath("/templates/entityResp.java.vm")//
+                // 生成API模块的Resp DTO
+                consumer.customFile(builder -> builder
+                    .templatePath("/templates/entityResp.java.vm")
                     .filePath(apiPath)
-                    .packageName("response")//
-                    .formatNameFunction(tableInfo -> tableInfo.getEntityName() + "Resp")//
-                    .fileName(".java")//
-                    .enableFileOverride()//
+                    .packageName("response")
+                    .formatNameFunction(tableInfo -> tableInfo.getEntityName() + "Resp")
+                    .fileName(".java")
+                    .enableFileOverride()
                 );
-                consumer.customFile(builder -> builder//
-                    .templatePath("/templates/feign.java.vm")//
+                // 生成API模块的Feign接口
+                consumer.customFile(builder -> builder
+                    .templatePath("/templates/feign.java.vm")
                     .filePath(apiPath)
-                    .packageName("feign")//
-                    .formatNameFunction(tableInfo -> tableInfo.getEntityName() + "Feign")//
-                    .fileName(".java")//
-                    .enableFileOverride()//
+                    .packageName("feign")
+                    .formatNameFunction(tableInfo -> tableInfo.getEntityName() + "Feign")
+                    .fileName(".java")
+                    .enableFileOverride()
+                );
+                // 生成AdminAPI模块的Controller
+                consumer.customFile(builder -> builder
+                    .templatePath("/templates/adminapi-controller.java.vm")
+                    .filePath(adminapiPath)
+                    .packageName("controller")
+                    .formatNameFunction(tableInfo -> tableInfo.getControllerName())
+                    .fileName(".java")
+                    .enableFileOverride()
+                );
+                // 生成AdminAPI模块的Excel DTO
+                consumer.customFile(builder -> builder
+                        .templatePath("/templates/entityExcel.java.vm")
+                        .filePath(adminapiPath)
+                        .packageName("excel")
+                        .formatNameFunction(tableInfo -> tableInfo.getEntityName() + "Excel")
+                        .fileName(".java")
+                        .enableFileOverride()
                 );
             })
             // 使用Velocity引擎模板
-            .templateEngine(new VelocityTemplateEngine()) // 默认VelocityTemplateEngine
+            .templateEngine(new VelocityTemplateEngine())
             .execute();
 
         log.info("代码生成完成，表名: {}，模块名: {}", tableName, moduleName);
-    }
-
-    /**
-     * 获取包名父级
-     *
-     * @param moduleName 模块名
-     * @return 包名父级
-     */
-    private String getPackageParent(String moduleName) {
-        switch (moduleName) {
-            case "adminapi":
-                return "com.company.adminapi";
-            case "admin":
-                return "com.company.admin";
-            case "system":
-                return "com.company.system";
-            case "order":
-                return "com.company.order";
-            case "user":
-                return "com.company.user";
-            case "tool":
-                return "com.company.tool";
-            default:
-                return "com.company." + moduleName;
-        }
-    }
-
-    /**
-     * 获取模块名称
-     *
-     * @param moduleName 模块名
-     * @return 模块名称
-     */
-    private String getModuleName(String moduleName) {
-        // 对于特定模块，我们不需要额外的模块名
-        List<String> noModuleNames = Arrays.asList("adminapi", "admin", "system", "order", "user", "tool");
-        if (noModuleNames.contains(moduleName)) {
-            return "";
-        }
-        return "";
     }
 }
