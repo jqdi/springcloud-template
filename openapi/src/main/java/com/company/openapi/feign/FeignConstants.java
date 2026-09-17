96ae30a89300065e8178b61c5c0a1595c7a5b730
JQ棣 <897665787@qq.com>
2026-09-17 14:09:26 +0800

    暂存

--- openapi/src/main/java/com/company/openapi/feign/FeignConstants.java
+++ openapi/src/main/java/com/company/openapi/feign/FeignConstants.java
@@ -7,5 +7,8 @@ public interface FeignConstants {
     /**
      * 服务名
      */
+    String TOOL = "template-tool";
+    String SYSTEM = "template-system";
+    String USER = "template-user";
     String ORDER = "template-order";
 }
